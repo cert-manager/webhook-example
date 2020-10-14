b0f67be14901429406436818880c75cc2a2aef06
Gun Store <ggunstore@gmail.com>
2020-10-14 22:00:28 +0100

    Update Name() to return Dynu Solver

--- main.go
+++ main.go
@@ -75,7 +75,7 @@ type customDNSProviderConfig struct {
 // within a single webhook deployment**.
 // For example, `cloudflare` may be used as the name of a solver.
 func (c *customDNSProviderSolver) Name() string {
-	return "my-custom-solver"
+	return "Dynu Solver"
 }
 
 // Present is responsible for actually presenting the DNS record with the
