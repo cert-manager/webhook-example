ba75aea4d19dc0b47140262afcefd158e9cc1cf3
roytev <roytev@gmail.com>
2022-02-08 14:03:18 +0200

    Comment once fix merged
    Signed-off-by: Roi Teveth <roytev@gmail.com>

--- main_test.go
+++ main_test.go
@@ -33,8 +33,9 @@ func TestRunsSuite(t *testing.T) {
 		dns.SetDNSServer("127.0.0.1:59351"),
 		dns.SetUseAuthoritative(false),
 	)
-
+	//need to uncomment and  RunConformance delete runBasic and runExtended once https://github.com/cert-manager/cert-manager/pull/4835 is merged
+	//fixture.RunConformance(t)
 	fixture.RunBasic(t)
 	fixture.RunExtended(t)
-	
+
 }
