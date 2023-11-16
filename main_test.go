4b9539815571bd41b886a01a95fdc633729bfd2f
Ashley Davis <ashley.davis@venafi.com>
2023-11-16 10:07:48 +0000

    bump deps to fix  CVE-2023-44487 / CVE-2023-39325
    Also requires an import change in the test as the import location was
    changed upstream in cert-manager.
    
    Signed-off-by: Ashley Davis <ashley.davis@venafi.com>

--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import (
 	"os"
 	"testing"
 
-	"github.com/cert-manager/cert-manager/test/acme/dns"
+	acmetest "github.com/cert-manager/cert-manager/test/acme"
 
 	"github.com/cert-manager/webhook-example/example"
 )
@@ -20,18 +20,18 @@ func TestRunsSuite(t *testing.T) {
 	//
 
 	// Uncomment the below fixture when implementing your custom DNS provider
-	//fixture := dns.NewFixture(&customDNSProviderSolver{},
-	//	dns.SetResolvedZone(zone),
-	//	dns.SetAllowAmbientCredentials(false),
-	//	dns.SetManifestPath("testdata/my-custom-solver"),
-	//	dns.SetBinariesPath("_test/kubebuilder/bin"),
+	//fixture := acmetest.NewFixture(&customDNSProviderSolver{},
+	//	acmetest.SetResolvedZone(zone),
+	//	acmetest.SetAllowAmbientCredentials(false),
+	//	acmetest.SetManifestPath("testdata/my-custom-solver"),
+	//	acmetest.SetBinariesPath("_test/kubebuilder/bin"),
 	//)
 	solver := example.New("59351")
-	fixture := dns.NewFixture(solver,
-		dns.SetResolvedZone("example.com."),
-		dns.SetManifestPath("testdata/my-custom-solver"),
-		dns.SetDNSServer("127.0.0.1:59351"),
-		dns.SetUseAuthoritative(false),
+	fixture := acmetest.NewFixture(solver,
+		acmetest.SetResolvedZone("example.com."),
+		acmetest.SetManifestPath("testdata/my-custom-solver"),
+		acmetest.SetDNSServer("127.0.0.1:59351"),
+		acmetest.SetUseAuthoritative(false),
 	)
 	//need to uncomment and  RunConformance delete runBasic and runExtended once https://github.com/cert-manager/cert-manager/pull/4835 is merged
 	//fixture.RunConformance(t)
