676466fe66a827ef1ef2acc8f0aad6d709fb5fc6
Tevildo <drwilko+gitlab@gmail.com>
2026-01-25 14:10:17 +0000

    Add/Remove A records with TTL

--- dns/dns.go
+++ dns/dns.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
 
 	"github.com/MartinWilkerson/cert-manager-webhook-nearlyfreespeech/auth"
@@ -31,6 +32,30 @@ func ClearTXTRecord(domain string, dnsName string, key string, login string, api
 	requestUrl := fmt.Sprintf("%s%s", baseUrl, urlPath)
 
 	values := url.Values{"name": {dnsName}, "type": {"TXT"}, "data": {key}}
+	body := values.Encode()
+	return send(login, apiKey, urlPath, body, requestUrl)
+}
+
+func SetARecord(domain string, subdomain string, data string, ttl uint32, login string, apiKey string) error {
+	urlPath := fmt.Sprintf("/dns/%s/addRR", domain)
+	requestUrl := fmt.Sprintf("%s%s", baseUrl, urlPath)
+	log.Printf("Request URL: %v", requestUrl)
+
+	values := url.Values{"name": {subdomain}, "type": {"A"}, "data": {data}, "ttl": {strconv.FormatUint(uint64(ttl), 10)}}
+	log.Printf("Body values: %v", values)
+
+	body := values.Encode()
+	return send(login, apiKey, urlPath, body, requestUrl)
+}
+
+func ClearARecord(domain string, subdomain string, data string, ttl uint32, login string, apiKey string) error {
+	urlPath := fmt.Sprintf("/dns/%s/removeRR", domain)
+	requestUrl := fmt.Sprintf("%s%s", baseUrl, urlPath)
+	log.Printf("Request URL: %v", requestUrl)
+
+	values := url.Values{"name": {subdomain}, "type": {"A"}, "data": {data}}
+	log.Printf("Body values: %v", values)
+
 	body := values.Encode()
 	return send(login, apiKey, urlPath, body, requestUrl)
 }
